4707d5fc965f7354739046f22e58e29fa11087d6
Abhinav Sharma <abhinav2061997@gmail.com>
2017-04-22 01:28:01 +0530

    full database update

--- authenticate.js
+++ authenticate.js
@@ -22,62 +22,61 @@ exports.google=passport.use(new GoogleStrategy({
         callbackURL: config.google.callbackURL
     },
     function(accessToken, refreshToken,profile, done) {
+        firsttime=2
         process.nextTick(function() {
-            User.findOne({ username: profile.emails[0].value }, function(err, user) {
-                if(err) {
-                    console.log(err); // handle errors!
-                }
-                if (!err && user !== null) {
-                    firsttime=0;
-                    listmessages(user,accessToken);
-                    done(null, user);
-                }
-                else {
-                    request({
-                            url: 'https://www.googleapis.com/gmail/v1/users/me/messages',
-                            method: 'GET',
-                            headers: {
-                                "Content-Type": "application/json",
-                                "Authorization": "Bearer "+accessToken
-                            },
-                            qs: {
-                                "maxResults":1
-                            }
-                        },
-                        function (err, response, body) {
-                            if(err){
-                                console.log(err); // Failure
-                            } else {
-                                body=JSON.parse(body);
-                                var messages=body.messages;
-                                latestmsg=messages[0].id;
-                                latestthread=messages[0].threadId;
-                                console.log(messages[0]);
-                                console.log(latestthread);
-                                user = new User({
-                                    username: profile.emails[0].value,
-                                    lastmsg : latestmsg,
-                                    lastthread :latestthread
-                                });
-                                user.save(function(err) {
-                                    if(err) {
-                                        console.log(err); // handle errors!
-                                    } else {
-                                        console.log("saving user ...");
-                                        firsttime=1;
-                                        listmessages(user,accessToken);
-                                        done(null, user);
-                                    }
-                                });
-                            }
-                        });
+        User.findOne({ username: profile.emails[0].value }, function(err, user) {
+            if(err) {
+                console.log(err); // handle errors!
+            }
+            if (!err && user !== null) {
+                firsttime=2;
+                 listmessages(user,accessToken,done);
 
-                }
-            });})
+            }
+            else {
+                request({
+                        url: 'https://www.googleapis.com/gmail/v1/users/me/messages',
+                        method: 'GET',
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Authorization": "Bearer "+accessToken
+                        },
+                        qs: {
+                            "maxResults":1
+                        }
+                    },
+                    function (err, response, body) {
+                        if(err){
+                            console.log(err); // Failure
+                        } else {
+                            body=JSON.parse(body);
+                            var messages=body.messages;
+                            latestmsg=messages[0].id;
+                            latestthread=messages[0].threadId;
+                            user = new User({
+                                username: profile.emails[0].value,
+                                lastmsg : latestmsg,
+                                lastthread :latestthread
+                            });
+                            user.save(function(err) {
+                                if(err) {
+                                    console.log(err); // handle errors!
+                                } else {
+                                    console.log("saving user ...");
+                                    firsttime=1;
+                                    listmessages(user,accessToken,done);
+
+                                }
+                            });
+                        }
+                    });
+
+                    }
+        });})
 
     }
 ));
-function listmessages(user,accessToken){
+function listmessages(user,accessToken,finalcallback){
     request({
             url: 'https://www.googleapis.com/gmail/v1/users/me/messages',
             method: 'GET',
@@ -94,8 +93,9 @@ function listmessages(user,accessToken){
                 console.log(err); // Failure
             } else {
                 var number;
-                var body=JSON.parse(body);
+                body=JSON.parse(body);
                 var messages=body.messages;
+                //console.log(messages);
                 if(firsttime!=1){
                     for(var i=0;messages[i];i++){
                         if(messages[i].threadId==user.lastthread&&messages[i].id==user.lastmsg){
@@ -107,48 +107,21 @@ function listmessages(user,accessToken){
                 }
                 else{
                     for(var i=0;messages[i];i++){
-                        number=i;
+                            number=i;
                     }
                 }
                 console.log(number);
-
-                for(var i=0;i<=number;i++){
-                    //console.log(messages[i].threadId);
-                    var currthread=messages[i].threadId;
-                    var currmsg=messages[i].id;
-
+                i=0;
+                if(i<number){
+                    updatedb(i,number,body,user,accessToken,finalcallback);
+                }
+                else{
                     User.findOne({ username: user.username }, function(err, updateuser) {
                         if(err) {
                             console.log(err); // handle errors!
                         }
-                        if (!err && updateuser !== null) {
-                            var newthread=0;
-                            //for checking
-                            console.log(updateuser.threads);
-                            for(var j=0;updateuser.threads[j];j++)
-                            {       console.log("yes");
-                                if(updateuser.threads[j].threadid==currthread){
-                                    console.log("yes");
-                                    //code for email body retrieve will come here
-                                    updateuser.threads[j].emails.push({emailid:currmsg});
-                                    newthread=1;
-                                    updateuser.save(function(err) {});
-                                    break;
-
-                                }
-                            }
-                            if(newthread!=1){
-                                var email={emailid:currmsg};
-                                updateuser.threads.push({threadid: currthread,emails:email});
-                                updateuser.save(function(err) {});}
-                        }
-                    });
-                }
-
-                if(body.nextPageToken){
-                    console.log("boop");
-                    nextpagetoken=response.nextPageToken;
-                    listnextmessagepage(user,gmail,oauth2Client,nextpagetoken);
+                        finalcallback(null,updateuser);
+                    })
 
                 }
             }
@@ -156,7 +129,7 @@ function listmessages(user,accessToken){
 
 
 }
-function listnextmessagepage(user,oauth2Client,nextpagetoken){
+function listnextmessagepage(user,accessToken,nextpagetoken,finalcallback){
 
     request({
             url: 'https://www.googleapis.com/gmail/v1/users/me/messages',
@@ -170,68 +143,105 @@ function listnextmessagepage(user,oauth2Client,nextpagetoken){
                 "pageToken":nextpagetoken
             }
         },
-        function (err, response, body) {
-            if(err){
-                console.log(err); // Failure
-            } else {
-                var number;
-                var body=JSON.parse(body);
-                var messages=body.messages;
-                if(firsttime!=1){
-                    for(var i=0;messages[i];i++){
-                        if(messages[i].threadId==user.lastthread&&messages[i].id==user.lastmsg){
-                            number=i;
-                            break;
-                        }
-                        else number=i;
-                    }
-                }
-                else{
-                    for(var i=0;messages[i];i++){
+        function (err, response, body) {if(err){
+            console.log(err); // Failure
+        } else {
+            var number;
+            body=JSON.parse(body);
+            var messages=body.messages;
+            if(firsttime!=1){
+                for(var i=0;messages[i];i++){
+                    if(messages[i].threadId==user.lastthread&&messages[i].id==user.lastmsg){
                         number=i;
+                        break;
                     }
-                }
-                console.log(number);
-
-                for(var i=0;i<=number;i++){
-                    //console.log(messages[i].threadId);
-                    var currthread=messages[i].threadId;
-                    var currmsg=messages[i].id;
-
-                    User.findOne({ username: user.username }, function(err, updateuser) {
-                        if(err) {
-                            console.log(err); // handle errors!
-                        }
-                        if (!err && updateuser !== null) {
-                            var newthread=0;
-                            //for checking
-                            console.log(updateuser.threads);
-                            for(var j=0;updateuser.threads[j];j++)
-                            {       console.log("yes");
-                                if(updateuser.threads[j].threadid==currthread){
-                                    console.log("yes");
-                                    //code for email body retrieve will come here
-                                    updateuser.threads[j].emails.push({emailid:currmsg});
-                                    newthread=1;
-                                    updateuser.save(function(err) {});
-                                    break;
-
-                                }
-                            }
-                            if(newthread!=1){
-                                var email={emailid:currmsg};
-                                updateuser.threads.push({threadid: currthread,emails:email});
-                                updateuser.save(function(err) {});}
-                        }
-                    });
-                }
-
-                if(response.nextPageToken){
-                    nextpagetoken=response.nextPageToken;
-                    listnextmessagepage(user,gmail,oauth2Client,nextpagetoken);
-
+                    else number=i;
                 }
             }
+            else{
+                for(var i=0;messages[i];i++){
+                    number=i;
+                }
+            }
+            console.log(number);
+            i=0;
+            if(i<number)
+                updatedb(i,number,body,user,accessToken,finalcallback);
+        }
+
         });
 
 }
+
+function updatedb(i,number,body,user,accessToken,finalcallback){
+        //console.log(messages[i].threadId);
+        var messages=body.messages;
+       // console.log(messages);
+        var currthread=messages[i].threadId;
+        var currmsg=messages[i].id;
+
+        User.findOne({ username: user.username }, function(err, updateuser) {
+            if(err) {
+                console.log(err); // handle errors!
+            }
+            if (!err && updateuser !== null) {
+                var newthread=0;
+                //for checking
+                //console.log(updateuser.threads);
+                for(var j=0;updateuser.threads[j];j++)
+                {
+                    if(updateuser.threads[j].threadid==currthread){
+                        //code for email body retrieve will come here
+                        updateuser.threads[j].emails.push({emailid:currmsg});
+                        newthread=1;
+                        updateuser.save(function(err) {
+                            if(i<number-1){
+                                i++;
+                                updatedb(i,number,body,user,accessToken,finalcallback);}
+                            if(!body.nextPageToken&&i==number-1){
+                                console.log("1");
+                                updateuser.lastmsg=messages[0].id;;
+                                updateuser.lastthread=messages[0].threadId;
+                                updateuser.save(function(err) {
+                                    finalcallback(null,updateuser);
+                                });
+                            }
+                            else if(body.nextPageToken&&i==number-1){
+                                console.log("2");
+                                var nextpagetoken=body.nextPageToken;
+                                listnextmessagepage(user,accessToken,nextpagetoken,finalcallback);
+                            }
+
+                        });
+                        break;
+                    }
+                }
+                if(newthread!=1){
+                    var email={emailid:currmsg};
+                    updateuser.threads.push({threadid: currthread,emails:email});
+                    updateuser.save(function(err) {
+                        if(!body.nextPageToken&&i==number-1){
+                            console.log("3");
+                            updateuser.lastmsg=messages[0].id;;
+                            updateuser.lastthread=messages[0].threadId;
+                            updateuser.save(function(err) {
+                                finalcallback(null,updateuser);
+                            });
+                        }
+                        else if(body.nextPageToken&&i==number-1){
+                            console.log("4");
+                            var nextpagetoken=body.nextPageToken;
+                            listnextmessagepage(user,accessToken,nextpagetoken,finalcallback);
+                        }
+                        if(i<number-1){i++;
+                            updatedb(i,number,body,user,accessToken,finalcallback);}
+
+                    });
+                }
+
+
+            }
+        });
+
+    }
+
